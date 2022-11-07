809ab7494572654a1aabe24b4054c45f8b7788d7
Eyerward <matthieubruffa@gmail.com>
2022-11-07 12:02:40 +0100

    la

--- Assets/Scripts/BonusAnim.cs
+++ Assets/Scripts/BonusAnim.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class BonusAnim : MonoBehaviour
 {
+    [SerializeField] GameObject platform;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void SpringFX()
+    {
+
     }
 
     // Update is called once per frame
